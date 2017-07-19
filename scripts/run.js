cb5054538abe29dadc688d57794c366f983ceb56
losfair <zhy20000919@hotmail.com>
2017-07-19 16:44:38 +0800

    rm

--- scripts/run.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-require("shadowsocks-plus");
--- src/backend/user.js
+++
@@ -1,9 +0,0 @@
-const assert = require("assert");
-
-export default class User {
-    constructor(userId, username) {
-        assert(typeof(userId) == "string" && typeof(username) == "string");
-        this.id = userId;
-        this.name = username;
-    }
-}
